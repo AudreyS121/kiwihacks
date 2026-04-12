1e80842360edd04c0f263b6ced57f6a761ebc972
Audrey Shi <audreyshi.121@gmail.com>
2026-04-13 10:31:30 +1200

    Began Branding Page
    - Drafted on .html
    - Added to "colours"

---
+++ src/branding/branding.jsx
@@ -0,0 +1,16 @@
+import "./branding.css";
+import Navbar from "../Navbar/Navbar";
+
+export default function branding() {
+  return (
+    <>
+      <Navbar />
+
+      <main className="brand-main">
+          <section className = "brand-header" >
+              
+          </section>
+      </main>
+    </>
+  );
+}
